87b96db1aee5ea66b69d00b8fb7f6b188eef5964
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-04-20 19:53:24 +0300

    feature: linkedlist - pop

---
+++ Linked List/Push.js
@@ -0,0 +1,108 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		const newNode = new Node(value);
+		this.head = newNode;
+		this.tail = this.head;
+		this.length = 1;
+	}
+
+
+	push(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		}
+		else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++
+		return this
+	}
+	pop() {
+		if (!this.head) return undefined;
+		let temp = this.head;
+		let pre = this.head;
+		while (temp.next) {
+			pre = temp;
+			temp = temp.next
+		}
+		this.tail = pre;
+		this.tail.next = null
+		this.length--
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return temp;
+	}
+	unshift(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		}
+		else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++
+		return this
+	}
+	shift() {
+		if (!this.head) return undefined;
+		let temp = this.head;
+		this.head = this.head.next
+		temp.next = null
+		this.length--
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return temp;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		let temp = this.head
+		for (let i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+}
+
+let myLinkedList = new LinkedList(7)
+myLinkedList.push(4);
+myLinkedList.push(3);
+myLinkedList.push(2);
+myLinkedList.push(1);
+myLinkedList.pop()
+myLinkedList.unshift(9)
+myLinkedList.shift()
+
+// myLinkedList.pop()
+console.log(myLinkedList);
+console.log(myLinkedList.get(2));
+
+
+/*
+	EXPECTED OUTPUT:
+	----------------
+	Head: 1
+	Tail: 2
+	Length: 2
+
+	Linked List:
+	1
+	2
+
+*/
